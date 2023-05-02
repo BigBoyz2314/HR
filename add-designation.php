a09f61b46b4f4d463b8987f13006a47a2f6bab0e
Zain <zazimran@gmail.com>
2023-05-02 12:14:45 +0500

    add designation done

---
+++ add-designation.php
@@ -0,0 +1,22 @@
+<?php
+    require_once('config.php');
+?>
+
+<?php
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_REQUEST["name"];
+        $grade = $_REQUEST["grade"];
+    }
+
+        $sql ="INSERT INTO designation VALUES ('', '$name', '$grade', current_timestamp())";
+        
+        if(mysqli_query($conn, $sql)){
+            echo $category;
+            header('Location: designation.php');
+        } else{
+            echo "ERROR: Hush! Sorry $sql. "
+            . mysqli_error($conn);
+        }
+
+?>
---
+++ department.php
@@ -0,0 +1,57 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" >
+    <title>Index</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="employees.php">Employees</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="department.php">Departments</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="designation.php">Designations</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Log out</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container-fluid">
+        
+    </div>
+    <script>
+        
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" ></script>    
+</body>
+</html>
