f38aaedcbb774f3a05748aecd82d62728f78ced7
Zain <zazimran@gmail.com>
2023-05-03 17:30:28 +0500

    changes

--- add-employee.php
+++ add-employee.php
@@ -47,12 +47,20 @@
         
     }
 
-        $sql ="INSERT INTO employees VALUES ('', '$fname', '$mname', '$lname',  '$dob', $designation, '', '', $department, '', '$mStatus', '$status', '$children', '$spousename', '$basicsalary', '$allowence', '$deduction', '$grosssalary', '$joindate', '$leavedate', '$paddress', '$saddress', '$caddress', '$pnumber', '$snumber', '$bank', '$bankacc', '$iban', '$manager', '', '$warnings', '$leaves', '$allowleaves', '$absents', '$presents', '$daysworking', '$loan', '$loanamount', '$cnic', '$passport', '$workinghours', '$starttime', '$endtime', '$shift', current_timestamp())";
+        $sql = "INSERT INTO `employees`(`employeeID`, `fname`, `mname`, `lname`, `dob`, `designation`, `designationID`, `grade`, `department`, `departmentID`, `martital_status`, `status`, `children`, `spouse_name`, `basic_salary`, `allowance`, `deduction`, `gross_salary`, `join_date`, `leave_date`, `primary_address`, `secondary_address`, `current_address`, `primary_number`, `secondary_number`, `bank_name`, `bank_account_no`, `iban`, `employee_code`, `manager_name`, `managerID`, `warnings`, `leaves`, `allowed_leaves`, `absents`, `presents`, `days_working`, `loan`, `loan_amount`, `cnic`, `passport_no`, `working_hours`, `start_time`, `end_time`, `shift`, `created_at`) VALUES
+         ('','$fname','$mname','$lname','$dob','$designation','','','$department','','$mStatus','$status','$children','$spousename','$basicsalary','$allowence','$deduction','$grosssalary','$joindate','$leavedate','$paddress','$saddress','$caddress','$pnumber','$snumber','$bank','$bankacc','$iban','','$manager','','$warnings','$leaves','$allowleaves','$absents','$presents','$daysworking','$loan','$loanamount','$cnic','$passport','$workinghours','$starttime','$endtime','$shift', current_timestamp())";
+        
+        
+        
+        
+        
+        
+        // "INSERT INTO employees VALUES ('', '$fname', '$mname', '$lname',  '$dob', $designation, '', '', $department, '', '$mStatus', '$status', '$children', '$spousename', '$basicsalary', '$allowence', '$deduction', '$grosssalary', '$joindate', '$leavedate', '$paddress', '$saddress', '$caddress', '$pnumber', '$snumber', '$bank', '$bankacc', '$iban', '$manager', '', '$warnings', '$leaves', '$allowleaves', '$absents', '$presents', '$daysworking', '$loan', '$loanamount', '$cnic', '$passport', '$workinghours', '$starttime', '$endtime', '$shift', current_timestamp())";
         
         if(mysqli_query($conn, $sql)){
 
             header('Location: employees.php');
-            
+
         } else{
             echo "ERROR: Hush! Sorry $sql. "
             . mysqli_error($conn);
