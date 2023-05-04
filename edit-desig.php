d1f0ad8f636d9a08a71b9541a579540cc22d2447
Zain <zazimran@gmail.com>
2023-05-04 12:34:11 +0500

    changes

---
+++ edit-desig.php
@@ -0,0 +1,23 @@
+<?php
+    require_once('config.php');
+?>
+
+<?php
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_REQUEST["name1"];
+        $grade = $_REQUEST["grade"];
+    }
+
+        $sql ="UPDATE `designation` SET `name`='$name', `grade`='$grade',`updated_at` = current_timestamp() WHERE `name` = '$name'";
+        
+        if(mysqli_query($conn, $sql)){
+
+            header('Location: designation.php');
+        
+        } else{
+            echo "ERROR: Hush! Sorry $sql. "
+            . mysqli_error($conn);
+        }
+
+?>
