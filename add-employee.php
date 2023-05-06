d8e0eb6a967522771cb2466210a80ab49b27c61e
Zain <zazimran@gmail.com>
2023-05-06 14:24:57 +0500

    changes

--- add-employee.php
+++ add-employee.php
@@ -68,10 +68,10 @@
             }
         }
 
-    
+        $yob = date('Y', strtotime($dob));
 
-        $sql = "INSERT INTO `employees`(`employeeID`, `fname`, `mname`, `lname`, `dob`,`gender`, `designation`, `designationID`, `grade`, `department`, `departmentID`, `martital_status`, `status`, `children`, `spouse_name`, `basic_salary`, `allowance`, `deduction`, `gross_salary`, `join_date`, `leave_date`, `primary_address`, `secondary_address`, `current_address`, `primary_number`, `secondary_number`, `bank_name`, `bank_account_no`, `iban`, `employee_code`, `manager_name`, `managerID`, `warnings`, `leaves`, `allowed_leaves`, `absents`, `presents`, `days_working`, `loan`, `loan_amount`, `cnic`, `passport_no`, `working_hours`, `start_time`, `end_time`, `shift`, `created_at`) VALUES
-         ('','$fname','$mname','$lname','$dob','$gender','$desig','$designation','$grade','$dept','$department','$mStatus','$status','$children','$spousename','$basicsalary','$allowence','$deduction','$grosssalary','$joindate','$leavedate','$paddress','$saddress','$caddress','$pnumber','$snumber','$bank','$bankacc','$iban','','$manager','','$warnings','$leaves','$allowleaves','$absents','$presents','$daysworking','$loan','$loanamount','$cnic','$passport','$workinghours','$starttime','$endtime','$shift', current_timestamp())";
+        $sql = "INSERT INTO `employees`(`employeeID`, `fname`, `mname`, `lname`, `dob`,`yob`,`gender`, `designation`, `designationID`, `grade`, `department`, `departmentID`, `martital_status`, `status`, `children`, `spouse_name`, `basic_salary`, `allowance`, `deduction`, `gross_salary`, `join_date`, `leave_date`, `primary_address`, `secondary_address`, `current_address`, `primary_number`, `secondary_number`, `bank_name`, `bank_account_no`, `iban`, `employee_code`, `manager_name`, `managerID`, `warnings`, `leaves`, `allowed_leaves`, `absents`, `presents`, `days_working`, `loan`, `loan_amount`, `cnic`, `passport_no`, `working_hours`, `start_time`, `end_time`, `shift`, `created_at`) VALUES
+         ('','$fname','$mname','$lname','$dob','$yob','$gender','$desig','$designation','$grade','$dept','$department','$mStatus','$status','$children','$spousename','$basicsalary','$allowence','$deduction','$grosssalary','$joindate','$leavedate','$paddress','$saddress','$caddress','$pnumber','$snumber','$bank','$bankacc','$iban','','$manager','','$warnings','$leaves','$allowleaves','$absents','$presents','$daysworking','$loan','$loanamount','$cnic','$passport','$workinghours','$starttime','$endtime','$shift', current_timestamp())";
         
         
         
--- view-employees.php
+++ view-employees.php
@@ -99,7 +99,7 @@ require_once('config.php');
                                         $mname = $row['mname'];
                                         $lname = $row['lname'];
                                         $dob = $row['dob'];
-                                        $desig = $row['designationID'];
+                                        $desig = $row['designation'];
                                         $dept = $row['department'];
                                         $gender = $row['gender'];
                                         $mstatus = $row['martital_status'];
@@ -140,6 +140,166 @@ require_once('config.php');
                                 $sta = $_GET['status'];
                             
                                 $stmt = "SELECT * FROM employees WHERE status = '$sta'";
+                                $result = $conn->query($stmt);
+                                $i = 1;
+        
+                                if ($result->num_rows > 0) {
+                                    // output data of each row
+                                    
+                                    while($row = $result->fetch_assoc()) { 
+                                        $id = $row['employeeID'];
+                                        $fname = $row['fname'];
+                                        $mname = $row['mname'];
+                                        $lname = $row['lname'];
+                                        $dob = $row['dob'];
+                                        $desig = $row['designation'];
+                                        $dept = $row['department'];
+                                        $gender = $row['gender'];
+                                        $mstatus = $row['martital_status'];
+                                        $status = $row['status'];
+                                        $children = $row['children'];
+                                        $spouse = $row['spouse_name'];
+                                        $basic = $row['basic_salary'];
+                                        $allowance = $row['allowance'];
+                                        $deduction = $row['deduction'];
+                                        $gross = $row['gross_salary'];
+
+                                        echo "<tr>";
+                                        echo "<td>". $i++ ."</td>";
+                                        echo "<td>$fname</td>";
+                                        echo "<td>$mname</td>";
+                                        echo "<td>$lname</td>";
+                                        echo "<td>". date("d M y", strtotime($dob)) ."</td>";
+                                        echo "<td>$desig</td>";
+                                        echo "<td>$gender</td>";
+                                        echo "<td>$dept</td>";
+                                        echo "<td>$mstatus</td>";
+                                        echo "<td>$status</td>";
+                                        // echo "<td>$children</td>";
+                                        echo "<td>$spouse</td>";
+                                        echo "<td>$basic</td>";
+                                        echo "<td>$allowance</td>";
+                                        echo "<td>$deduction</td>";
+                                        echo "<td>$gross</td>";
+                                        echo "<td><form action='view-emp.php' method='get'><input type='hidden' name='id' value='". $id ."'><input type='submit' value='View Details' class='btn btn-info'></form></td>";
+                                        // echo "<td><form action='' method='get'><input type='hidden' name='desigName' value='". $name ."'><input type='hidden' name='id' value='". $id ."'><input type='submit' value='Delete' class='btn btn-danger'></form></td>";
+                                        echo "</tr>";
+        
+                                    }
+                                }
+                            }
+                            elseif (isset($_GET['dept'])) {
+                                
+                                $deptid = $_GET['dept'];
+                            
+                                $stmt = "SELECT * FROM employees WHERE departmentID = '$deptid'";
+                                $result = $conn->query($stmt);
+                                $i = 1;
+        
+                                if ($result->num_rows > 0) {
+                                    // output data of each row
+                                    
+                                    while($row = $result->fetch_assoc()) { 
+                                        $id = $row['employeeID'];
+                                        $fname = $row['fname'];
+                                        $mname = $row['mname'];
+                                        $lname = $row['lname'];
+                                        $dob = $row['dob'];
+                                        $desig = $row['designation'];
+                                        $dept = $row['department'];
+                                        $gender = $row['gender'];
+                                        $mstatus = $row['martital_status'];
+                                        $status = $row['status'];
+                                        $children = $row['children'];
+                                        $spouse = $row['spouse_name'];
+                                        $basic = $row['basic_salary'];
+                                        $allowance = $row['allowance'];
+                                        $deduction = $row['deduction'];
+                                        $gross = $row['gross_salary'];
+
+                                        echo "<tr>";
+                                        echo "<td>". $i++ ."</td>";
+                                        echo "<td>$fname</td>";
+                                        echo "<td>$mname</td>";
+                                        echo "<td>$lname</td>";
+                                        echo "<td>". date("d M y", strtotime($dob)) ."</td>";
+                                        echo "<td>$desig</td>";
+                                        echo "<td>$gender</td>";
+                                        echo "<td>$dept</td>";
+                                        echo "<td>$mstatus</td>";
+                                        echo "<td>$status</td>";
+                                        // echo "<td>$children</td>";
+                                        echo "<td>$spouse</td>";
+                                        echo "<td>$basic</td>";
+                                        echo "<td>$allowance</td>";
+                                        echo "<td>$deduction</td>";
+                                        echo "<td>$gross</td>";
+                                        echo "<td><form action='view-emp.php' method='get'><input type='hidden' name='id' value='". $id ."'><input type='submit' value='View Details' class='btn btn-info'></form></td>";
+                                        // echo "<td><form action='' method='get'><input type='hidden' name='desigName' value='". $name ."'><input type='hidden' name='id' value='". $id ."'><input type='submit' value='Delete' class='btn btn-danger'></form></td>";
+                                        echo "</tr>";
+        
+                                    }
+                                }
+                            }
+                            elseif (isset($_GET['from']) || isset($_GET['to'])) {
+                                
+                                $from = $_GET['from'];
+                                $to = $_GET['to'];
+                            
+                                $stmt = "SELECT * FROM employees WHERE yob BETWEEN $from AND $to";
+                                $result = $conn->query($stmt);
+                                $i = 1;
+        
+                                if ($result->num_rows > 0) {
+                                    // output data of each row
+                                    
+                                    while($row = $result->fetch_assoc()) { 
+                                        $id = $row['employeeID'];
+                                        $fname = $row['fname'];
+                                        $mname = $row['mname'];
+                                        $lname = $row['lname'];
+                                        $dob = $row['dob'];
+                                        $desig = $row['designation'];
+                                        $dept = $row['department'];
+                                        $gender = $row['gender'];
+                                        $mstatus = $row['martital_status'];
+                                        $status = $row['status'];
+                                        $children = $row['children'];
+                                        $spouse = $row['spouse_name'];
+                                        $basic = $row['basic_salary'];
+                                        $allowance = $row['allowance'];
+                                        $deduction = $row['deduction'];
+                                        $gross = $row['gross_salary'];
+
+                                        echo "<tr>";
+                                        echo "<td>". $i++ ."</td>";
+                                        echo "<td>$fname</td>";
+                                        echo "<td>$mname</td>";
+                                        echo "<td>$lname</td>";
+                                        echo "<td>". date("d M y", strtotime($dob)) ."</td>";
+                                        echo "<td>$desig</td>";
+                                        echo "<td>$gender</td>";
+                                        echo "<td>$dept</td>";
+                                        echo "<td>$mstatus</td>";
+                                        echo "<td>$status</td>";
+                                        // echo "<td>$children</td>";
+                                        echo "<td>$spouse</td>";
+                                        echo "<td>$basic</td>";
+                                        echo "<td>$allowance</td>";
+                                        echo "<td>$deduction</td>";
+                                        echo "<td>$gross</td>";
+                                        echo "<td><form action='view-emp.php' method='get'><input type='hidden' name='id' value='". $id ."'><input type='submit' value='View Details' class='btn btn-info'></form></td>";
+                                        // echo "<td><form action='' method='get'><input type='hidden' name='desigName' value='". $name ."'><input type='hidden' name='id' value='". $id ."'><input type='submit' value='Delete' class='btn btn-danger'></form></td>";
+                                        echo "</tr>";
+        
+                                    }
+                                }
+                            }
+                            elseif (isset($_GET['desig'])) {
+                                
+                                $deptid = $_GET['desig'];
+                            
+                                $stmt = "SELECT * FROM employees WHERE designationID = '$deptid'";
                                 $result = $conn->query($stmt);
                                 $i = 1;
         
