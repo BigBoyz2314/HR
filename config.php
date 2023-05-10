82cb724721c6cd5776b4e38283d595fb2af1ab51
Zain <zazimran@gmail.com>
2023-05-10 18:08:29 +0500

    changes

--- config.php
+++ config.php
@@ -4,12 +4,11 @@ $username = "root";
 $password = "";
 $dbname = "hr";
 
-// Create connection
+
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connecting
+
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-// echo "Connected successfully";
