9fa034ceedf22d41cf7125633220b3e708696f73
Zain <zazimran@gmail.com>
2023-05-18 12:00:41 +0500

    changes

--- view-emp.php
+++ view-emp.php
@@ -55,6 +55,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     $shift = $row['shift'];
     $created = $row['created_at'];
     $updated = $row['updated_at'];
+    $gender = $row['gender'];
 
 ?>
 <!DOCTYPE html>
@@ -70,207 +71,227 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <body>
 <?php include 'nav.php' ?>
     <div class="container-fluid p-5">
-        <h1>View Employee</h1>
+        <div class="row justify-content-between">
+            <div class="col-md-3">
+                <h1>View Employee</h1>
+            </div>
+            <div class="col-md-3">
+                    <h5>Created</h5>
+                    <input value="<?php echo $created?>" disabled type="text" name="created" id="created" class="form-control">
+                </div>
+                <div class="col-md-3">
+                    <h5>Updated</h5>
+                    <input value="<?php echo $updated?>" disabled type="text" name="created" id="created" class="form-control">
+                </div>    
+                <div class="col-md-2">
+                    <h5>Employee ID</h5>
+                    <input value="<?php echo $eid?>" disabled type="text" name="eid" id="eid" class="form-control">
+                </div>    
+        </div>
         <form action="" method="post">
-            <div class="row justify-content-center">
-                <div class="col-md-3 pt-4">
-                    <h4>First Name</h4>
+            <div class="row">
+                <div class="col-md-2 pt-4">
+                    <h5>First Name</h5>
                     <input value="<?php echo $fname?>" disabled type="text" name="fName" id="fName" class="form-control">
                 </div>
-                <div class="col-md-3 pt-4">
-                    <h4>Middle Name</h4>
+                <div class="col-md-2 pt-4">
+                    <h5>Middle Name</h5>
                     <input value="<?php echo $mname?>" disabled type="text" name="mName" id="mName" class="form-control">
                 </div>
-                <div class="col-md-3 pt-4">
-                    <h4>Last Name</h4>
+                <div class="col-md-2 pt-4">
+                    <h5>Last Name</h5>
                     <input value="<?php echo $lname?>" disabled type="text" name="lName" id="lName" class="form-control">
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-md-3 pt-4">
-                    <h4>Date of Birth</h4>
+                <div class="col-md-2 pt-4">
+                    <h5>Gender</h5>
+                    <input value="<?php echo $gender?>" disabled type="text" name="lName" id="lName" class="form-control">
+                </div>
+                <div class="col-md-2 pt-4">
+                    <h5>Date of Birth</h5>
                     <input value="<?php echo $dob?>" disabled type="text" name="dob" id="dob" class="form-control">
                 </div>
-                <div class="col-md-3 pt-4">
-                    <h4>Martital Status</h4>
+                <div class="col-md-2 pt-4">
+                    <h5>Martital Status</h5>
                     <input value="<?php echo $mstatus?>" disabled name="mStatus" id="mStatus" class="form-control">
                 </div>
-                <div class="col-md-3 pt-4">
-                    <h4>Children</h4>
+            </div>
+            <div class="row">
+                <div class="col-md-2 pt-4">
+                    <h5>Children</h5>
                     <input value="<?php echo $children?>" disabled type="number" min="0" name="children" id="children" class="form-control">
                 </div>
-                <div class="col-md-3 pt-4">
-                    <h4>Spouse Name</h4>
+                <div class="col-md-2 pt-4">
+                    <h5>Spouse Name</h5>
                     <input value="<?php echo $spouse?>" disabled type="text" name="spouseName" id="spouseName" class="form-control">
                 </div>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-md-4 pt-4">
-                    <h4>CNIC</h4>
+                <div class="col-md-2 pt-4">
+                    <h5>CNIC</h5>
                     <input value="<?php echo $cnic?>" disabled type="number" name="cnic" id="cnic" class="form-control">
                 </div>
-                <div class="col-md-4 pt-4">
-                    <h4>Passport no.</h4>
+                <div class="col-md-2 pt-4">
+                    <h5>Passport</h5>
                     <input value="<?php echo $passport?>" disabled type="text" name="passport" id="passport" class="form-control">
+                </div>
+                <div class="col-md-2 pt-4">
+                    <h5>Disabled</h5>
+                    <input value="" disabled type="text" name="passport" id="passport" class="form-control">
+                </div>
+                <div class="col-md-2 pt-4">
+                    <h5>Photo</h5>
+                    <input value="" disabled type="text" name="passport" id="passport" class="form-control">
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-4 pt-4">
-                    <h4>Primary Address</h4>
+                    <h5>Primary Address</h5>
                     <input value="<?php echo $paddress?>" disabled type="text" name="pAddress" id="pAddress" class="form-control">
                 </div>
                 <div class="col-md-4 pt-4">
-                    <h4>Secondary Address</h4>
+                    <h5>Secondary Address</h5>
                     <input value="<?php echo $saddress?>" disabled type="text" name="sAddress" id="sAddress" class="form-control">
                 </div>
                 <div class="col-md-4 pt-3">
-                    <h4 class="d-inline-block">Current Address</h4>
+                    <h5 class="d-inline-block">Current Address</h5>
                     <input value="<?php echo $caddress?>" disabled type="text" name="cAddress" id="cAddress" class="form-control">
                 </div>
             </div>
-            <div class="row justify-content-center">
+            <div class="row">
                 <div class="col-md-4 pt-4">
-                    <h4>Primary Number</h4>
+                    <h5>Contact 1 #</h5>
                     <input value="<?php echo $pnumber?>" disabled type="number" name="pNumber" id="pNumber" class="form-control">
                 </div>
                 <div class="col-md-4 pt-4">
-                    <h4>Secondary Number</h4>
+                    <h5>Contact 2 #</h5>
                     <input value="<?php echo $snumber?>" disabled type="number" name="sNumber" id="sNumber" class="form-control">
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-3 pt-4">
-                    <h4>Basic Salary</h4>
+                    <h5>Basic Salary</h5>
                     <input value="<?php echo $basic?>" disabled type="number" min="0" name="basicSalary" id="basicSalary" class="form-control">
                 </div>
                 <div class="col-md-3 pt-4">
-                    <h4>Allowence</h4>
+                    <h5>Allowence</h5>
                     <input value="<?php echo $allowance?>" disabled type="number" min="0" name="allowence" id="allowence" class="form-control">
                 </div>
                 <div class="col-md-3 pt-4">
-                    <h4>Deduction</h4>
+                    <h5>Deduction</h5>
                     <input value="<?php echo $deduction?>" disabled type="number" min="0" name="deduction" id="deduction" class="form-control">
                 </div>
                 <div class="col-md-3 pt-4">
-                    <h4>Gross Salary</h4>
+                    <h5>Gross Salary</h5>
                     <input value="<?php echo $gross?>" disabled type="number" name="grossSalary" id="grossSalary" class="form-control">
-                </div>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-md-4 pt-4">
-                    <h4>Designation</h4>
-                    <input value="<?php echo $desig?>" disabled type="text" name="designation" id="designation" class="form-control">
-                </div>
-                <div class="col-md-4 pt-4">
-                    <h4>Department</h4>
-                    <input value="<?php echo $dept?>" disabled type="text" name="department" id="department" class="form-control">
-                </div>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-md-3 pt-4">
-                    <h4>Join Date</h4>
-                    <input value="<?php echo $joindate?>" disabled type="text" name="joinDate" id="joinDate" class="form-control">
-                </div>
-                <div class="col-md-3 pt-4">
-                    <h4>Leave Date</h4>
-                    <input value="<?php echo $leavedate?>" disabled type="text" name="leaveDate" id="leaveDate" class="form-control">
-                </div>
-                <div class="col-md-3 pt-4">
-                    <h4>Created</h4>
-                    <input value="<?php echo $created?>" disabled type="text" name="created" id="created" class="form-control">
-                </div>
-                <div class="col-md-3 pt-4">
-                    <h4>Updated</h4>
-                    <input value="<?php echo $updated?>" disabled type="text" name="created" id="created" class="form-control">
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-3 pt-4">
-                    <h4>Bank Name</h4>
+                    <h5>Bank Name</h5>
                     <input value="<?php echo $bank?>" disabled type="text" name="bank" id="bank" class="form-control">
                 </div>
                 <div class="col-md-4 pt-4">
-                    <h4>Bank Account no.</h4>
+                    <h5>Bank Account no.</h5>
                     <input value="<?php echo $bankacc?>" disabled type="number" name="bankAcc" id="bankAcc" class="form-control">
                 </div>
                 <div class="col-md-5 pt-4">
-                    <h4>IBAN</h4>
+                    <h5>IBAN</h5>
                     <input value="<?php echo $iban?>" disabled type="text" name="iban" id="iban" class="form-control">
                 </div>
             </div>
-            <div class="row justify-content-center">
-                <div class="col-md-3 pt-4">
-                    <h4>Manager Name</h4>
-                    <input value="<?php echo $manager?>" disabled type="text" name="manager" id="manager" class="form-control">
-                </div>
-                <div class="col-md-3 pt-4">
-                    <h4>Warnings</h4>
-                    <input value="<?php echo $warnings?>" disabled type="number" min="0" name="warnings" id="warnings" class="form-control">
-                </div>
-                <div class="col-md-3 pt-4">
-                    <h4>Leaves</h4>
-                    <input value="<?php echo $leaves?>" disabled type="number" min="0" name="leaves" id="leaves" class="form-control">
-                </div>
-                <div class="col-md-3 pt-4">
-                    <h4>Allowed Leaves</h4>
-                    <input value="<?php echo $allowleave?>" disabled type="number" min="0" name="allowLeaves" id="allowLeaves" class="form-control">
-                </div>
-            </div>
-            <div class="row justify-content-center">
+            <div class="row">
                 <div class="col-md-2 pt-4">
-                    <h4>Absents</h4>
-                    <input value="<?php echo $absents?>" disabled type="number" min="0" name="absents" id="absents" class="form-control">
+                    <h5>Designation</h5>
+                    <input value="<?php echo $desig?>" disabled type="text" name="designation" id="designation" class="form-control">
                 </div>
                 <div class="col-md-2 pt-4">
-                    <h4>Presents</h4>
-                    <input value="<?php echo $presents?>" disabled type="number" min="0" name="presents" id="presents" class="form-control">
-                </div>
-                <div class="col-md-3 pt-4">
-                    <h4>Days Workings</h4>
-                    <input value="<?php echo $daysworking?>" disabled type="number" min="0" name="daysWorking" id="daysWorking" class="form-control">
+                    <h5>Department</h5>
+                    <input value="<?php echo $dept?>" disabled type="text" name="department" id="department" class="form-control">
                 </div>
                 <div class="col-md-2 pt-4">
-                    <h4>Loan</h4>
-                    <input value="<?php echo $loan?>" disabled name="loan" id="loan" class="form-control">
-                </div>
-                <div class="col-md-3 pt-4">
-                    <h4>Loan Amount</h4>
-                    <input value="<?php echo $loanamount?>" disabled type="number" min="0" name="loanAmount" id="loanAmount" class="form-control">
-                </div>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-md-2 pt-4">
-                    <h4>Working Hours</h4>
-                    <input value="<?php echo $daysworking?>" disabled type="number" min="0" name="workingHours" id="workingHours" class="form-control">
-                </div>
-                <div class="col-md-3 pt-4">
-                    <h4>Start Time</h4>
-                    <input value="<?php echo $starttime?>" disabled type="text" name="startTime" id="startTime" class="form-control">
-                </div>
-                <div class="col-md-3 pt-4">
-                    <h4>End Time</h4>
-                    <input value="<?php echo $endtime?>" disabled type="text" name="endTime" id="endTime" class="form-control">
+                    <h5>Join Date</h5>
+                    <input value="<?php echo $joindate?>" disabled type="text" name="joinDate" id="joinDate" class="form-control">
                 </div>
                 <div class="col-md-2 pt-4">
-                    <h4>Shift</h4>
+                    <h5>Leave Date</h5>
+                    <input value="<?php echo $leavedate?>" disabled type="text" name="leaveDate" id="leaveDate" class="form-control">
+                </div>
+                <div class="col-md-2 pt-4">
+                    <h5>Shift</h5>
                     <input value="<?php echo $shift?>" disabled name="shift" id="shift" class="form-control">
                 </div>
                 <div class="col-md-2 pt-4">
-                    <h4>Status</h4>
+                    <h5>Status</h5>
                     <input value="<?php echo $status?>" disabled name="status" id="status" class="form-control">
+                </div>
             </div>
-        </form>
+            <div class="row">
+                
+            </div>
+            <div class="row">
+                <div class="col-md-2 pt-4">
+                    <h5>Line Manager</h5>
+                    <input value="<?php echo $manager?>" disabled type="text" name="manager" id="manager" class="form-control">
+                </div>
+                <div class="col-md-2 pt-4">
+                    <h5>Warnings</h5>
+                    <input value="<?php echo $warnings?>" disabled type="number" min="0" name="warnings" id="warnings" class="form-control">
+                </div>
+                <div class="col-md-2 pt-4">
+                    <h5>Leaves</h5>
+                    <input value="<?php echo $leaves?>" disabled type="number" min="0" name="leaves" id="leaves" class="form-control">
+                </div>
+                <div class="col-md-2 pt-4">
+                    <h5>Allowed Leaves</h5>
+                    <input value="<?php echo $allowleave?>" disabled type="number" min="0" name="allowLeaves" id="allowLeaves" class="form-control">
+                </div>
+                <div class="col-md-2 pt-4">
+                    <h5>Absents</h5>
+                    <input value="<?php echo $absents?>" disabled type="number" min="0" name="absents" id="absents" class="form-control">
+                </div>
+                <div class="col-md-2 pt-4">
+                    <h5>Presents</h5>
+                    <input value="<?php echo $presents?>" disabled type="number" min="0" name="presents" id="presents" class="form-control">
+                </div>
+            </div>
+            <div class="row">
+                
+                <div class="col-md-2 pt-4">
+                    <h5>Days Workings</h5>
+                    <input value="<?php echo $daysworking?>" disabled type="number" min="0" name="daysWorking" id="daysWorking" class="form-control">
+                </div>
+                <div class="col-md-2 pt-4">
+                    <h5>Loan</h5>
+                    <input value="<?php echo $loan?>" disabled name="loan" id="loan" class="form-control">
+                </div>
+                <div class="col-md-2 pt-4">
+                    <h5>Loan Amount</h5>
+                    <input value="<?php echo $loanamount?>" disabled type="number" min="0" name="loanAmount" id="loanAmount" class="form-control">
+                </div>
+                <div class="col-md-2 pt-4">
+                    <h5>Hours</h5>
+                    <input value="<?php echo $daysworking?>" disabled type="number" min="0" name="workingHours" id="workingHours" class="form-control">
+                </div>
+                <div class="col-md-2 pt-4">
+                    <h5>Start Time</h5>
+                    <input value="<?php echo $starttime?>" disabled type="text" name="startTime" id="startTime" class="form-control">
+                </div>
+                <div class="col-md-2 pt-4">
+                    <h5>End Time</h5>
+                    <input value="<?php echo $endtime?>" disabled type="text" name="endTime" id="endTime" class="form-control">
+                </div>
+            </div>
+            </form>
+            <div class="row justify-content-center">
         <?php
         
         if ($_SESSION['role'] == '1') {
-            echo '<form action="edit-employee.php" method="get" class="mt-4">
+            echo '<form action="edit-employee.php" method="get" class="mt-4 ">
                     <input type="hidden" name="empID" id="empID" value="'. $id .'">
                     <input type="submit" value="Edit" class="btn btn-warning">
                 </form>';
         }
 
         ?>
-        
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" ></script>
     <script>
