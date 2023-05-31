17d69867230d530eed2a8795e19db9ce728ad600
Zain <zazimran@gmail.com>
2023-05-31 11:15:22 +0500

    changes

--- uploads/generate-salary copy.php
+++ uploads/generate-salary copy.php
@@ -17,10 +17,9 @@
     $thismonth = date("n");
     $thisyear = date("Y");
     $date = date("d");
-    $dayspayable = $date;
-    if ($date == 31) {
-        $dayspayable = 30;
-    }
+    $firstDate = date("Ymd", strtotime($today));
+    $secondDates = $_GET['year'] .'-0'. $_GET['month'];
+    $secondDate = date("Ym", strtotime($secondDates));
 
     if ($result->num_rows > 0) {
         // output data of each row
@@ -39,16 +38,27 @@
             $deduction = $row['deduction'];
             $gross = $row['gross_salary'];                                      
             
-            $day = 365/12;
+            $day = 30;
+
+            if ($_GET['month'] == 2) {
+
+                $day = 28;
+
+            }
 
             $dpay = $gross / $day;
 
             $abs = $day - $date;
 
-            if ($date == 31) {
+            if ($_GET['month'] == 2 && $date == 28) {
 
                 $abs = 0;
-            
+
+            }
+            elseif ($date == 31) {
+
+                $abs = 0;
+
             }
 
             $absent = $abs * $dpay;
@@ -58,13 +68,20 @@
             if ($date == 31) {
 
                 $pay = $dpay * $day;
+                $date = 30;
+
+            }
+
+            else {
+
+                $pay = $dpay * $day;
 
             }
 
             $pay = $pay - $absent;
 
             $sql ="INSERT INTO `salary`(`employeeID`,`fname`, `mname`, `lname`, `designation`, `department`, `gender`, `basic_salary`, `allowance`, `deduction`, `gross_salary`, `month`, `year`, `total_days`, `pay_days`, `absent`, `payable`, `paid`, `remaining`, `created_at`, `updated_at`)
-            VALUES ('$id','$fname','$mname','$lname','$desig','$dept','$gender','$basic','$allowance','$deduction','$gross','$month','$year','$totaldays','$dayspayable','$absent','$pay','','',current_timestamp(),current_timestamp())";
+            VALUES ('$id','$fname','$mname','$lname','$desig','$dept','$gender','$basic','$allowance','$deduction','$gross','$month','$year','$totaldays','$date','$absent','$pay','','',current_timestamp(),current_timestamp())";
             
             mysqli_query($conn, $sql);
         }
